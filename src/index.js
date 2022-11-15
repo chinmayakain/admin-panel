5cb32d5f2199fa7c68dd49aff8a7de736c3501aa
chinmayakain <chinmaynaik.009@gmail.com>
2022-11-16 02:25:16 +0530

    fix: :zap: build fix
    build fix

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
+        <meta
+            http-equiv="Content-Security-Policy"
+            content="upgrade-insecure-requests"
+        ></meta>
         <App />
     </React.StrictMode>
 );
